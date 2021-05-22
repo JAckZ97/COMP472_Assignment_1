ec547ee17cb75116a8585c4be211d4f52dac1c68
JAckZ97 <zzj1353832196@gmail.com>
2021-05-21 22:36:48 -0400

    update todo list

--- mapGenerate.py
+++ mapGenerate.py
@@ -2,13 +2,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # TODO:
- 
+# label the area name with text window
+# label the node with letters, if all letter runs out, use "AA, AB etc"
+# start thinking the heuristic algorithm and implementation
+
 numRow = input("Enter the number of rows: ")
 numColumn = input("Enter the number of columns: ")
 numRow = int(numRow)
 numColumn = int(numColumn)
-# numRow = 2
-# numColumn = 3
 
 node_x = []
 node_y = []
@@ -36,18 +37,7 @@ for y in range(0,numColumn+1):
     edge.append((1 , 1+(numRow)*0.1))
     edge.append('r')
 
-# print(edge)
-
-# plt.title('Assignment_1')
-
-# node_x = [1, 1.2, 1.4, 1.6, 1, 1.2, 1.4, 1.6, 1, 1.2, 1.4, 1.6]
-# node_y = [1, 1, 1, 1, 1.1, 1.1, 1.1, 1.1, 1.2, 1.2, 1.2, 1.2]
-
-# area_x = [1.1, 1.3, 1.5, 1.1, 1.3, 1.5]
-# area_y = [1.05, 1.05, 1.05, 1.15, 1.15, 1.15]
-
-# edge=[(1, 1.6), (1, 1),'r', (1, 1.6), (1.1, 1.1),'r', (1, 1.6), (1.2, 1.2),'r', 
-#     (1, 1), (1, 1.2),'r', (1.2, 1.2), (1, 1.2),'r', (1.4, 1.4), (1, 1.2),'r', (1.6, 1.6), (1, 1.2),'r']
+plt.title('Assignment_1')
 
 plt.plot(*edge)
 plt.scatter(node_x, node_y, marker='o')
